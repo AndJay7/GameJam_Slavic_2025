f60e01bb78674d5ae19897a8b7a043a003539181
MiDz5 <kotbudyn5@gmail.com>
2025-08-02 11:16:15 +0200

    Layer changes and mirror attack

---
+++ Assets/Scripts/Survivor/MirrorAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    namespace Survivor
+{
+    public class MirrorAttack : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        if (PlayerMovement.Instance.LatestMovementDirection.x > 0)
+            {
+            transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+
+        // Update is called once per frame
+        /*
+        void Update()
+            {
+
+                {
+                    if (PlayerMovement.Instance.LatestMovementDirection.x > 0)
+                    {
+
+                        transform.localScale = new Vector3(-1, 1, 1);
+                    }
+                    if (PlayerMovement.Instance.LatestMovementDirection.x < 0)
+                    {
+                        transform.localScale = new Vector3(1, 1, 1);
+
+                    }
+                }
+
+
+            }
+     */
+    }
+}
